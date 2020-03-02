f66dfd02ffd6a937ada486bf0f1bb8c7a4163e49
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-03-02 22:03:22 +0100

    some more plot

--- gen-quandl.lisp
+++ gen-quandl.lisp
@@ -9,8 +9,12 @@
   (let* ((code
 	  `(do0
 	    "#!/usr/bin/env python3"
-	    
+	    "# pip3 install --user mplfinance"
 	    (do0
+	     (do0
+	      (imports ((plt matplotlib.pyplot)
+			(mpf mplfinance)))
+	      (plt.ion))
 	     (imports (quandl
 		       datetime
 		       calendar
@@ -18,8 +22,24 @@
 		       (np numpy)
 		       time
 		       pathlib
-		       re)))
+		       re))
 
+	     (do0
+	      (setf f (open (string "p"))
+		   pw (dot f
+			   (read)
+			   (replace (string "\\n")
+				    (string ""))))
+	      (f.close))
+
+	     (setf quandl.ApiConfig.api_key pw)
+	     (setf d (quandl.get (string "EURONEXT/ASML")
+				 :start_data (string "2019-03-02")
+				 )
+		   d (d.rename :columns (dict ((string "Last")
+					       (string "Close")))))
+	     (mpf.plot d :type (string "candle")
+		       :volume True))
 	    ))
 	 )
     (write-source (format nil "~a/source/run_01_quandl_get_stock" *path*)
--- source/run_01_quandl_get_stock.py
+++ source/run_01_quandl_get_stock.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# pip3 install --user mplfinance
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+plt.ion()
 import quandl
 import datetime
 import calendar
@@ -7,3 +11,10 @@ import numpy as np
 import time
 import pathlib
 import re
+f=open("p")
+pw=f.read().replace("\n", "")
+f.close()
+quandl.ApiConfig.api_key=pw
+d=quandl.get("EURONEXT/ASML", start_data="2019-03-02")
+d=d.rename(columns={("Last"):("Close")})
+mpf.plot(d, type="candle", volume=True)
