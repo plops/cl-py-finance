4409e5e5dbb5098b2ab2c5b759f39da4715c321e
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-07-16 21:53:50 +0200

    get an example

--- gen_nasdaq_poll.lisp
+++ gen_nasdaq_poll.lisp
@@ -22,12 +22,14 @@
 		     requests
 		     json)))
 	  (do0
-	   (setf symbols (list ,@(loop for e in `(LITE NVDA AMD ASML INTC BDX) collect
+	   (setf symbols (list ,@(loop for e in `(TSLA LITE NVDA AMD ASML INTC BDX) collect
 				     `(string ,e))))
 
 
-	   (def get_ticks (symbol &key (time (string "00:00"))
+	   (def get_ticks (symbol &key (timestamp (string "00:00"))
 				  (limit 100))
+	     (print (dot (string "requesting last {} trades for {} ")
+			 (format limit symbol)))
 	    (setf headers (dict ,@(loop for (k v) in `((authority api.nasdaq.com)
 						       (accept "application/json, text/plain, */*")
 						       (user-agent "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36")
@@ -44,7 +46,7 @@
 				(tuple (string "limit") (dot (string "{}")
 							     (format limit)) ; (string "20")
 				       )
-				(tuple (string "fromTime") time ;(string "00:00")
+				(tuple (string "fromTime") timestamp ;(string "00:00")
 				       )))
 
 	    
@@ -52,16 +54,33 @@
 					      (format symbol))
 					 :headers headers
 					 :params params))
+	    ;(print response.text)
 	    (setf ticks (json.loads response.text))
-	    (setf df (dot (pd.DataFrame (aref (aref ticks (string "data"))
+	    (setf df #+nil (dot (pd.DataFrame (aref (aref ticks (string "data"))
 					     (string "rows")))
-			  (set_index (string "nlsTime"))))
+			  (set_index (string "nlsTime")))
+		  (pd.DataFrame (aref (aref ticks (string "data"))
+				      (string "rows"))))
 	    (setf (aref df (string "request_time")) (pd.Timestamp.now))
 	    (return df))
-	   (def run (&key (time (string "00:00")) (limit 20))
+	   (def run (&key (timestamp (string "00:00")) (limit 20))
 	     ;(print (json.dumps (get_ticks (aref symbols 0) :time time) :indent 2))
-	     (print (get_ticks (aref symbols 0) :time time :limit limit))
+	     (print (get_ticks (aref symbols 0) :timestamp timestamp :limit limit))
 	     )
+
+	   (setf symb (aref symbols 0))
+	   (setf a (get_ticks symb :limit 100))
+	   (time.sleep 5)
+	   (setf b (get_ticks symb :limit 100))
+
+	   (setf compare_cols (aref a.columns (list 0 1 2)))
+	   (comments "find the row in b that is identical to the most recent row in a") 
+	   (setf first_identical
+		 (aref b
+		       (dot (== (aref b compare_cols)
+				(aref (dot a (aref iloc 0))
+				      compare_cols))
+			    (all :axis 1))))
 	   )
 	  ))
        )
--- source/run_02_nasdaq_poll.py
+++ source/run_02_nasdaq_poll.py
@@ -9,14 +9,22 @@ import pathlib
 import re
 import requests
 import json
-symbols=["LITE", "NVDA", "AMD", "ASML", "INTC", "BDX"]
-def get_ticks(symbol, time="00:00", limit=100):
+symbols=["TSLA", "LITE", "NVDA", "AMD", "ASML", "INTC", "BDX"]
+def get_ticks(symbol, timestamp="00:00", limit=100):
+    print("requesting last {} trades for {} ".format(limit, symbol))
     headers={("authority"):("api.nasdaq.com"),("accept"):("application/json, text/plain, */*"),("user-agent"):("Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36"),("origin"):("https://www.nasdaq.com"),("sec-fetch-site"):("same-site"),("sec-fetch-mode"):("cors"),("sec-fetch-dest"):("empty"),("referer"):("https://www.nasdaq.com/market-activity/stocks/lite/latest-real-time-trades"),("accept-language"):("en-US,en;q=0.9")}
-    params=(("","",),("limit","{}".format(limit),),("fromTime",time,),)
+    params=(("","",),("limit","{}".format(limit),),("fromTime",timestamp,),)
     response=requests.get("https://api.nasdaq.com/api/quote/{}/realtime-trades".format(symbol), headers=headers, params=params)
     ticks=json.loads(response.text)
-    df=pd.DataFrame(ticks["data"]["rows"]).set_index("nlsTime")
+    df=pd.DataFrame(ticks["data"]["rows"])
     df["request_time"]=pd.Timestamp.now()
     return df
-def run(time="00:00", limit=20):
-    print(get_ticks(symbols[0], time=time, limit=limit))
+def run(timestamp="00:00", limit=20):
+    print(get_ticks(symbols[0], timestamp=timestamp, limit=limit))
+symb=symbols[0]
+a=get_ticks(symb, limit=100)
+time.sleep(5)
+b=get_ticks(symb, limit=100)
+compare_cols=a.columns[[0, 1, 2]]
+# find the row in b that is identical to the most recent row in a
+first_identical=b[((b[compare_cols])==(a.iloc[0][compare_cols])).all(axis=1)]
