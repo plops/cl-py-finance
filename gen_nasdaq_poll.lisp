e67df3dd588ddec68aa3389d5401ef088481ac16
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-07-16 18:51:29 +0200

    create request header

--- gen_nasdaq_poll.lisp
+++ gen_nasdaq_poll.lisp
@@ -22,6 +22,21 @@
 	  (do0
 	   (setf stocks (list ,@(loop for e in `(LITE NVDA AMD ASML INTC BDX) collect
 				     `(string ,e))))
+	   (setf headers (dict ,@(loop for (k v) in `((authority api.nasdaq.com)
+						      (accept "application/json, text/plain, */*")
+						      (user-agent "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36")
+						      (origin "https://www.nasdaq.com")
+						      (sec-fetch-site "same-site")
+						      (sec-fetch-mode  "cors")
+						      (sec-fetch-dest "empty")
+						      (referer 
+						       "https://www.nasdaq.com/market-activity/stocks/lite/latest-real-time-trades")
+						      (accept-language "en-US,en;q=0.9"))
+				    collect
+				      `((string ,k) (string ,v)))))
+	   (setf params (tuple (tuple (string "") (string ""))
+			       (tuple (string "limit") (string "20"))
+			       (tuple (string "fromTime") (string "00:00"))))
 	   )
 	  ))
        )
--- source/run_02_nasdaq_poll.py
+++ source/run_02_nasdaq_poll.py
@@ -8,3 +8,5 @@ import pathlib
 import re
 import requests
 stocks=["LITE", "NVDA", "AMD", "ASML", "INTC", "BDX"]
+headers={("authority"):("api.nasdaq.com"),("accept"):("application/json, text/plain, */*"),("user-agent"):("Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36"),("origin"):("https://www.nasdaq.com"),("sec-fetch-site"):("same-site"),("sec-fetch-mode"):("cors"),("sec-fetch-dest"):("empty"),("referer"):("https://www.nasdaq.com/market-activity/stocks/lite/latest-real-time-trades"),("accept-language"):("en-US,en;q=0.9")}
+params=(("","",),("limit","20",),("fromTime","00:00",),)
